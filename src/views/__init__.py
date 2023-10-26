57f4bebb14ef538456f36610b6d39c034be6a7b2
AndresAlba <o.alba@uniandes.edu.co>
2023-10-26 10:51:35 -0500

    Test personal information

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,1 +1,1 @@
-from .views import *
+from .view_personal import *
--- src/views/view_personal.py
+++ src/views/view_personal.py
@@ -2,9 +2,7 @@ import uuid
 from flask_restful import Resource
 from flask import request
 from datetime import datetime, timedelta
-from flask_jwt_extended import create_access_token, decode_token
 from ..models import db, PersonalInformation, PersonalSchema
-from ..utils import encrypt
 
 personal_schema = PersonalSchema()
 
